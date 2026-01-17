d85de8b91a8640cfa13b68b9813db171d3d0dff0
Alex Ru <alexander.rudel@web.de>
2026-01-17 19:10:39 +0300

    GameShape Struct
    Created Struct to handle shapes in game and draw them on the screen

--- BouncyShapes/src/Main.cpp
+++ BouncyShapes/src/Main.cpp
@@ -7,11 +7,76 @@
 
 #include<SFML/Graphics.hpp>
 
+//function declaration
 void pollEvent(sf::RenderWindow& window);
-void loadFromFile(const char* filename);
+void loadFromFile(const char* filename, std::vector<GameShape*>& shapeVector);
 
+//Structs and Classes
+struct GameShape{
+
+    sf::Shape* shapeType;
+    bool draw { true };
+    std::string name{};
+    float speedX{};
+    float speedY{};
+    float colorF[3] {};
+    uint8_t colorI[3] {};
+    float scaler{ 1 };
+
+    GameShape(std::string name, float posX, float posY, float speedX, float speedY, float r, float g, float b, float width, float height):
+    name(name),
+    speedX(speedX),
+    speedY(speedY){
+        colorI[0] = r;
+        colorI[1] = g;
+        colorI[2] = b;
+        colorItoF();
+        shapeType = new sf::RectangleShape({width, height});
+        shapeType->setOrigin(shapeType->getGeometricCenter());
+        shapeType->setPosition({ posX, posY });
+        shapeType->setFillColor({colorI[0], colorI[1], colorI[2]});
+        std::cout << "Created Rect at: " << posX << ' ' << posY << '\n';
+    }
+    GameShape(std::string name, float posX, float posY, float speedX, float speedY, float r, float g, float b, float radius):
+    name(name),
+    speedX(speedX),
+    speedY(speedY){
+        colorI[0] = r;
+        colorI[1] = g;
+        colorI[2] = b;
+        colorItoF();
+        shapeType = new sf::CircleShape(radius);
+        shapeType->setOrigin(shapeType->getGeometricCenter());
+        shapeType->setPosition({ posX, posY });
+        shapeType->setFillColor({ colorI[0], colorI[1], colorI[2] });
+        std::cout << "Created Circle at: " << posX << ' ' << posY << '\n';
+    }
+    ~GameShape() {
+        std::cout << "Called Shape destructor\n";
+        delete shapeType;
+        shapeType = nullptr;
+    };
+
+    void colorItoF() {
+        colorF[0] = uint8_t(colorI[0] * 255);
+        colorF[1] = uint8_t(colorI[1] * 255);
+        colorF[2] = uint8_t(colorI[2] * 255);
+    }
+    void colorFtoI() {
+        colorI[0] = float(colorF[0] / 255);
+        colorI[1] = float(colorF[1] / 255);
+        colorI[2] = float(colorF[2] / 255);
+    }
+    void printContent() {
+        std::cout << name << ": " << shapeType << " Color float: " << colorF[0] << colorF[1] << colorF[2] << " Color uInt: " << colorI[0] << colorI[1] << colorI[2] << '\n';
+    }
+
+};
+
+//Main
 int main() {
-
+    
+    std::vector<GameShape*> shapesInGame;
     unsigned int width = 1280;
     unsigned int height = 760;
 
@@ -25,14 +90,18 @@ int main() {
 
     sf::Clock deltaClock;
 
-    loadFromFile("config/config.txt");
+    loadFromFile("config/config.txt", shapesInGame);
 
     //scale the imgui ui and text size by 2
     ImGui::GetStyle().ScaleAllSizes(2.0f);
     ImGui::GetIO().FontGlobalScale = 2.0f;
 
     while (window.isOpen()) {
+        //Event handling
         pollEvent(window);
+
+        //Physics Update
+
 
         //Update ImGui
         ImGui::SFML::Update(window, deltaClock.restart());
@@ -45,6 +114,10 @@ int main() {
         //Clear Window
         window.clear();
         //Draw window
+        for (auto s : shapesInGame) {
+            window.draw(*s->shapeType);
+        }
+        
         ImGui::SFML::Render(window);
         //Display Window
         window.display();
@@ -54,6 +127,7 @@ int main() {
     return 0;
 }
 
+//Function Definitions
 void pollEvent(sf::RenderWindow& window) {
     while (const auto event = window.pollEvent()) {
         ImGui::SFML::ProcessEvent(window, *event);
@@ -69,7 +143,7 @@ void pollEvent(sf::RenderWindow& window) {
     }
 }
 
-void loadFromFile(const char* filename) {
+void loadFromFile(const char* filename, std::vector<GameShape*>& shapeVector) {
     std::ifstream fin(filename);
     std::string temp;
 
@@ -84,11 +158,31 @@ void loadFromFile(const char* filename) {
             std::cout << "A Font \n";
         }
         else if (temp == "Circle") {
-            std::cout << "A circle \n";
+            std::string name;
+            float posX;
+            float posY;
+            float speedX;
+            float speedY;
+            float r;
+            float g;
+            float b;
+            float radius;
+            fin >> name >> posX >> posY >> speedX >> speedY >> r >> g >> b >> radius;
+            shapeVector.push_back(new GameShape(name, posX, posY, speedX, speedY, r, g, b, radius));
         }
         else if (temp == "Rectange") {
-            std::cout << "A Rectangle \n";
+            std::string name;
+            float posX;
+            float posY;
+            float speedX;
+            float speedY;
+            float r;
+            float g;
+            float b;
+            float width;
+            float height;
+            fin >> name >> posX >> posY >> speedX >> speedY >> r >> g >> b >> width >> height;
+            shapeVector.push_back(new GameShape(name, posX, posY, speedX, speedY, r, g, b, width, height));
         }
     }
-
 }
